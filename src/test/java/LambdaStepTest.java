30e96eccc46697226c580ea36b109a84ce9b0037
David-Koper <kuznetsov.david87@gmail.com>
2022-07-10 17:09:42 +0300

    Added LambdaStepTest

---
+++ src/test/java/LambdaStepTest.java
@@ -0,0 +1,41 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class LambdaStepTest {
+
+    private final String Repository = "eroshenkoam/allure-example";
+    private final Integer Issue_Number = 76;
+
+    @Test
+    public void testGitHubIssueSearch() {
+        //SelenideLogger.addListener("allure",new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com/");});
+
+        step("Ищем репозиторий по имени" + Repository, () -> {
+            $(".js-site-search-form").click();
+            $(".input-sm").sendKeys(Repository);
+            $(".input-sm").submit();});
+
+        step("В результатах поиска переходим по ссылке репозитория " + Repository, () -> {
+            $(By.linkText("eroshenkoam/allure-example")).click();});
+
+        step("Открываем таб Issues", () -> {
+        $(By.partialLinkText("Issues")).click();});
+
+        step("Проверяем что существует Issue c номером " + Issue_Number, () -> {
+        $(withText("#" + Issue_Number)).should(Condition.exist);});
+
+
+    }
+
+}
